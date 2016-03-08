085b8513d67640ccdc14dc053ac159e64609bef7
Kristo Prifti <klp110@aubg.bg>
2016-03-08 17:37:08 +0200

    first commit

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ShoppingListApplication.java
@@ -0,0 +1,21 @@
+package com.udacity.firebase.shoppinglistplusplus;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Includes one-time initialization of Firebase related code
+ */
+public class ShoppingListApplication extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        /* Initialize Firebase */
+        Firebase.setAndroidContext(this);
+        // TODO The application code is where all "setup" code should go.
+        // Consider putting your off-line code here.
+        /* Enable disk persistence  */
+        Firebase.getDefaultConfig().setPersistenceEnabled(true);
+    }
+
+}
